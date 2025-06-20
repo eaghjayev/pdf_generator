dcc77ad1125eac9f0a2cbb7f391b843d9a29f945
eaghjayev <eaghjayev2018@outlook.com>
2025-06-20 15:08:06 +0400

    Add lines

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=topic, align='L', ln=1)
-    pdf.line(10, 20, 200, 22)
+    for num in range(20, 298, 10):
+        pdf.line(10, num, 200, num)
+
 
     # Set the footer
     pdf.ln(265)
@@ -25,10 +27,10 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=topic, align='R')
 
-
     for i in range(pages-1):
         pdf.add_page()
-
+        for num in range(20, 298, 10):
+            pdf.line(10, num, 200, num)
 
         # Set the footer
         pdf.ln(277)
